081dcf7ae26d6dfeeb70acc9352a1dc93579a251
Jeg Ramos <jegramos.pa@gmail.com>
2019-04-25 17:58:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <v-layout row justify-center>
+          <v-flex xs12 sm10 md8>
+            <app-http></app-http>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Http from './components/http-feature/Http.vue'
+
+export default {
+  name: 'App',
+  components: {
+    appHttp: Http
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/components/http-feature/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-toolbar dark color="red" class="mb-3" app dense>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <h4>MEVN SPRINT: <span class="font-weight-light">CRUD APP</span></h4>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>ADD FACILITY</v-btn>
+        <v-btn @click="listFacilities" flat>LIST FACILITIES</v-btn>
+        <v-btn flat>EDIT FACILITY</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    listFacilities () {
+      this.$emit('listFacilities', true)
+    }
+  }
+}
+</script>
